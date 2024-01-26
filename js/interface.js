90277c59289f6a23149449608de3d65d1dc19a4e
Veljko Popović <veljko_popovic_90@hotmail.com>
2024-01-26 09:56:07 +0100

    file list

--- js/interface.js
+++ js/interface.js
@@ -1,75 +1,103 @@
-var appDataSources = [];
+// var appDataSources = [];
 
-Fliplet.DataSources.get({
-  appId: Fliplet.Env.get('masterAppId'),
-  attributes: ['id', 'name']
-}).then(function(dataSources) {
-  appDataSources = dataSources.map((el) => {
-    return { value: el.id, label: el.name };
-  });
-  Fliplet.Widget.generateInterface({
-    title: 'File list',
-    fields: [
-      {
-        name: 'dataSourceTemp',
-        type: 'provider',
-        label: 'Datasource',
-        package: 'com.fliplet.data-source-provider'
-      },
-      {
-        name: 'dataSource',
-        type: 'dropdown',
-        label: 'Data Source',
-        options: appDataSources,
-        default: '',
-        change: function(value) {
-          Fliplet.Helper.field('columnName').toggle(value);
+// Fliplet.DataSources.get({
+//   appId: Fliplet.Env.get('masterAppId'),
+//   attributes: ['id', 'name']
+// }).then(function(dataSources) {
+//   appDataSources = dataSources.map((el) => {
+//     return { value: el.id, label: el.name };
+//   });
+Fliplet.Widget.generateInterface({
+  title: 'File list',
+  fields: [
+    {
+      name: 'dataSource',
+      type: 'provider',
+      label: 'Datasource',
+      package: 'com.fliplet.data-source-provider',
+      change: function(value) {
+        Fliplet.Helper.field('columnName').toggle(value);
 
-          if (value) {
-            Fliplet.DataSources.getById(value, {
-              attributes: ['columns']
-            }).then(function(columns) {
-              $('#columnName').html('');
-              columns.columns.forEach((el) => {
-                $('#columnName').append(`<option value="${el}">${el}</option>`);
-              });
+        if (value) {
+          Fliplet.DataSources.getById(value, {
+            attributes: ['columns']
+          }).then(function(columns) {
+            $('#columnName').html('');
+            columns.columns.forEach((el) => {
+              $('#columnName').append(`<option value="${el}">${el}</option>`);
             });
-          }
-        },
-        ready: function() {
-          var value = Fliplet.Helper.field('dataSource').get();
-
-          if (value) {
-            Fliplet.DataSources.getById(value, {
-              attributes: ['columns']
-            }).then(function(columns) {
-              $('#columnName').html('');
-              columns.columns.forEach((el) => {
-                $('#columnName').append(`<option value="${el}">${el}</option>`);
-              });
-            });
-          }
+          });
         }
       },
-      {
-        name: 'columnName',
-        type: 'dropdown',
-        label: 'Pick file column',
-        options: [],
-        default: '',
-        ready: function() {
-          Fliplet.Helper.field('columnName').toggle(
-            Fliplet.Helper.field('dataSource').get()
-          );
+      ready: function() {
+        var value = Fliplet.Helper.field('dataSource').get();
+
+        if (value) {
+          Fliplet.DataSources.getById(value, {
+            attributes: ['columns']
+          }).then(function(columns) {
+            $('#columnName').html('');
+            columns.columns.forEach((el) => {
+              $('#columnName').append(`<option value="${el}">${el}</option>`);
+            });
+          });
         }
-      },
-      {
-        name: 'type',
-        type: 'dropdown',
-        label: 'Type',
-        options: ['Image', 'File'],
-        default: 'Image'
       }
-    ]
-  });
+    },
+    // {
+    //   name: 'dataSource',
+    //   type: 'dropdown',
+    //   label: 'Data Source',
+    //   options: appDataSources,
+    //   default: '',
+    //   change: function(value) {
+    //     Fliplet.Helper.field('columnName').toggle(value);
+
+    //     if (value) {
+    //       Fliplet.DataSources.getById(value, {
+    //         attributes: ['columns']
+    //       }).then(function(columns) {
+    //         $('#columnName').html('');
+    //         columns.columns.forEach((el) => {
+    //           $('#columnName').append(`<option value="${el}">${el}</option>`);
+    //         });
+    //       });
+    //     }
+    //   },
+    //   ready: function() {
+    //     var value = Fliplet.Helper.field('dataSource').get();
+
+    //     if (value) {
+    //       Fliplet.DataSources.getById(value, {
+    //         attributes: ['columns']
+    //       }).then(function(columns) {
+    //         $('#columnName').html('');
+    //         columns.columns.forEach((el) => {
+    //           $('#columnName').append(`<option value="${el}">${el}</option>`);
+    //         });
+    //       });
+    //     }
+    //   }
+    // },
+    {
+      name: 'columnName',
+      type: 'dropdown',
+      label: 'Pick file column',
+      options: [],
+      default: '',
+      ready: function() {
+        Fliplet.Helper.field('columnName').toggle(
+          Fliplet.Helper.field('dataSource').get()
+        );
+      }
+    },
+    {
+      name: 'type',
+      type: 'dropdown',
+      label: 'Type',
+      options: ['Image', 'File'],
+      default: 'Image'
+    }
+  ]
 });
+// });
